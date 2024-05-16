0cc4f2bc2eaeaea3dbdebbc17983fff1d0a89b48
Adham Ahmed <adham2272001@gmail.com>
2024-05-17 02:52:02 +0300

    Added camera icon

--- src/components/ProfileCardPersonal.tsx
+++ src/components/ProfileCardPersonal.tsx
@@ -1,9 +1,28 @@
 import AvatarPicture from "../assets/avatar.png";
+import { Box, IconButton } from "@mui/material";
+import CameraAltOutlinedIcon from "@mui/icons-material/CameraAltOutlined";
+
 const ProfileCardPersonal = () => {
   return (
     <div className="p-4">
       <div className="flex justify-center h-[120px] w-[120px] bg-white">
-        <img src={AvatarPicture} alt="profile" className="rounded-[40px]" />
+        <Box position="relative" display="inline-block">
+          <img src={AvatarPicture} alt="profile" className="rounded-[40px]" />
+          <IconButton
+            sx={{
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              color: "#2D2F30",
+              backgroundColor: "#F0F0F0",
+              "&:hover": {
+                backgroundColor: "gray.200",
+              },
+            }}
+          >
+            <CameraAltOutlinedIcon />
+          </IconButton>
+        </Box>
       </div>
       <div className="pt-4">
         <h1 className="text-2xl font-bold">John Smith</h1>
