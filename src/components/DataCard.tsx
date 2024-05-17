09f2e0fd6229569a6f1fff10118adcc161515746
Adham Ahmed <adham2272001@gmail.com>
2024-05-17 20:49:09 +0300

    setup OpenGL

--- src/components/DataCard.tsx
+++ src/components/DataCard.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import DataCardForm from "./DataCardForm";
 import DataCardHeader from "./DataCardHeader";
 
-
 const DataCard = () => {
   const [editable, setEditable] = useState(false);
   return (
--- src/components/DataCardRail.tsx
+++ src/components/DataCardRail.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import DataCard from "./DataCard";
-
+import { useQuery } from "@apollo/client";
+import { GET_USER_PROFILE } from "../graphql/Queries";
 const DataCardRail = () => {
+  const { data } = useQuery(GET_USER_PROFILE);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  // console.log(user);
   return (
     <div className="flex flex-row w-[1155px] h-[1718px] bg-[#FAFAFA] space-x-6">
       <DataCard></DataCard>
